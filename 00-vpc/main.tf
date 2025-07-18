4808b7afd9aa1ef8f7f0e070831d48a4f3276947
vvramadm <vvramadm@gmail.com>
2025-07-18 19:58:13 +0530

    helm-eks

--- 00-vpc/main.tf
+++ 00-vpc/main.tf
@@ -5,7 +5,6 @@ module "vpc" {
     environment = var.environment
     vpc_cidr = var.vpc_cidr
     common_tags = var.common_tags
-    #vpc_tags = var.vpc_tags
     public_subnet_cidrs = var.public_subnet_cidrs
     private_subnet_cidrs = var.private_subnet_cidrs
     database_subnet_cidrs = var.database_subnet_cidrs
